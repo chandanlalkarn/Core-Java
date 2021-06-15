98d5ad30791e25fc045ff6b5616560fc55b68a9c
chandanlalkarn <chandan.lalkarn@gmail.com>
2021-06-15 16:38:32 +0530

    Add files via upload

---
+++ SizeTest.java
@@ -0,0 +1,8 @@
+class SizeTest{
+public static void main(String[] om){
+short s1 = 10000;
+short s2 = 60000; //it gives error which is expected.
+System.out.println("Vlaue of s1"+s1);
+System.out.println("Vlaue of s2"+s2);
+}
+}
